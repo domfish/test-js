1fe1d20fe877275e28000caafb1ad379579a1b2b
Chiheb mejri <chihebmjrr@gmail.com>
2022-07-05 11:09:05 +0100

    +

--- exercice1.js
+++ exercice1.js
@@ -1,8 +1,31 @@
-function sommepos(arr){
-   var somme =0;
-    arr.sort()
-    somme = arr[0]+arr[1]
-    console.log(somme);
+// function sommepos(arr){
+//    var somme =0;
+//     arr.sort()
+//     somme = arr[0]+arr[1]
+//     console.log(somme);
    
+// }
+// sommepos([10, 343445353, 3453445, 3453545353453])
+
+function smallest_pair(arr)
+{
+  let min = Number.MAX_VALUE,
+  secondMin = Number.MAX_VALUE;
+  for (let j = 0; j < arr.length; j++)
+  {
+    if (arr[j] < min)
+    {
+      secondMin = min;
+      min = arr[j];
+    }
+    else if ((arr[j] < secondMin) &&
+        arr[j] != min)
+      secondMin = arr[j];
+  }
+  var sum = secondMin + min
+  
+  console.log(sum);
 }
-sommepos([10, 343445353, 3453445, 3453545353453])
+smallest_pair([1,2,3]); 
+
+
